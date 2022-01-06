8c6a2edd4478b42f8e874e9d027131a157b255ec
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-05 17:30:38 -0700

    console log practice

--- Javascript_Intro_Class/script.js
+++ Javascript_Intro_Class/script.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+console.log("akdlkfsdlwel");
+
+console.log(545);
+
+console.log("6464");
+
+var location = "I'm going" + " to the store";
+
+var string1 ='Time to ';
+
+var string2 = 'go to bed.';
+
+var string3 = string1 + string2;
+
+console.log(string3);
+
+var random_string = "al;sdkfja;lskejr";
+
+console.log(random_string.length);
