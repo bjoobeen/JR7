fe62e3c36e179a5a8d7c0207b6dfb8ff2f56563b
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-07 10:05:31 -0700

    in class ex

---
+++ Javascript_Intro/inClassExercise1_5_2022.js
@@ -0,0 +1,21 @@
+//inClass Ex1
+
+var hoursOfSleep = 3
+
+if (hoursOfSleep <= 6) {
+    console.log("I am groggy.");
+} else {
+    console.log("I feel fantastic?")
+}
+
+//inClass Ex2
+
+var nextLocation = "home"
+
+if (nextLocation === "home") {
+    console.log("It's been a long day, let's go home!");
+} else if (nextLocation === "work") {
+    console.log("Good morning, finding the fastest route to work!"); 
+} else {
+    console.log("Good morning, finding the fastest route!");
+}
