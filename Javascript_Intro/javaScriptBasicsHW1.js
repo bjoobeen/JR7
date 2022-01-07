bdffc82dcb40daddf7aab2f75f33489e1ec34b37
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-07 10:25:03 -0700

    Ex1Part2(a)

--- Javascript_Intro/javaScriptBasicsHW1.js
+++ Javascript_Intro/javaScriptBasicsHW1.js
@@ -1,27 +1,39 @@
-//Ex1 a.)
+//Ex1 (a - d) 
+
+// (a)
 var num1 = 5;
 var num2 = 10;
 
 var numAdd = num1 + num2;
 console.log(numAdd);
 
-//Ex1 b.)
+// (b)
 var num3 = 12;
 var num4 = 15;
 
 var numSub = num4 - num3;
 console.log(numSub);
 
-//Ex1 c.)
+// (c)
 var num5 = 20;
 var num6 = 40;
 
 var numDiv = (num6 / num5);
 console.log(numDiv);
 
-//Ex1 d.)
+// (d)
 var num7 = 25;
 var num8 = 10;
 
 var numMult = num7 * num8;
 console.log(numMult);
+
+//Ex2 (a - b)
+
+// (a)
+
+var string1 = "Hello";
+var string2 = " World";
+
+var concat = string1 + string2;
+console.log(concat);
