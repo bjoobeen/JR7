d4f38b282ef50ea6c62f50aba7a39c01dc19e856
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-07 11:12:19 -0700

    completed ex4

---
+++ Javascript_Intro/javaScriptVarEx.js
@@ -0,0 +1,38 @@
+//Variable Group Ex
+
+// (1)
+var int = 5;
+console.log(int);
+
+// (2)
+var flt = 2.7193;
+console.log(flt);
+
+// (3)
+var string1 = "Are you a string??";
+console.log(string1);
+
+// (4)
+var int2 = 10;
+var dbleFloat = 2.5;
+
+// (4.a) Add numbers
+ var add = int2 + dbleFloat;
+ console.log(add);
+
+ // (4.b) Divide numbers
+ var divide = dbleFloat / int2;
+console.log(divide);
+
+// (4.c) Multiply numbers
+var multiply = int2 * dbleFloat;
+console.log(multiply);
+
+// (4.d) Subtract numbers
+var subtract = int2 - dbleFloat;
+console.log(subtract);
+
+// (4.e) find the remainder
+var remainder = dbleFloat % int2;
+console.log(remainder);
+
