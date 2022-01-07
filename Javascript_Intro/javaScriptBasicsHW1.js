6e04768d9cb539b13df2b293ddec68018b7fa6c7
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-07 10:18:20 -0700

    Ex1 completed

---
+++ Javascript_Intro/javaScriptBasicsHW1.js
@@ -0,0 +1,27 @@
+//Ex1 a.)
+var num1 = 5;
+var num2 = 10;
+
+var numAdd = num1 + num2;
+console.log(numAdd);
+
+//Ex1 b.)
+var num3 = 12;
+var num4 = 15;
+
+var numSub = num4 - num3;
+console.log(numSub);
+
+//Ex1 c.)
+var num5 = 20;
+var num6 = 40;
+
+var numDiv = (num6 / num5);
+console.log(numDiv);
+
+//Ex1 d.)
+var num7 = 25;
+var num8 = 10;
+
+var numMult = num7 * num8;
+console.log(numMult);
