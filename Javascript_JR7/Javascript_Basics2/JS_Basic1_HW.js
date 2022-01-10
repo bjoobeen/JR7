0e819bd39b20c28eec28da8854fa34bd40fd27c9
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-10 10:52:27 -0700

    homework completed

---
+++ Javascript_JR7/Javascript_Basics2/JS_Basic1_HW.js
@@ -0,0 +1,46 @@
+//Grade Calculator
+
+function myGradeFunction(x) {
+    if (x <= 98) {
+        return "A+";
+    }
+    else if (x >= 92) {
+        return "A";
+    }
+    else if (x >= 90) {
+        return "A-";
+    }
+    else if (x >= 88) {
+        return "B+";
+    }
+    else if (x >= 82) {
+        return "B";
+    }
+    else if (x >= 80) {
+        return "B-";
+    }
+    else if (x >= 78) {
+        return "C+";
+    }
+    else if (x >= 72) {
+        return "C";
+    }
+    else if (x >= 70) {
+        return "C-";
+    }
+    else if (x >= 68) {
+        return "D+";
+    }
+    else if (x >= 62) {
+        return "D";
+    }
+    else if (x >= 62) {
+        return "D-";
+    }
+    else (x < 60) {
+        return "F";
+    }
+}
+
+var result = myGradeFunction();
+console.log(result);
