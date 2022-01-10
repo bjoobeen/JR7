52ab3d68beaeaf216935fc841e9497c0b40f3829
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-10 10:36:40 -0700

    completed ex2

---
+++ Javascript_JR7/Javascript_Basics2/inClassJavascriptBasic2Ex2.js
@@ -0,0 +1,17 @@
+//Ex1
+function myFunction() {
+    return console.log("Wow, it worked!");
+}
+myFunction();
+
+//Ex2
+function favMovie() {
+    return console.log("My favorite movie is Kagemusha.");
+}
+favMovie();
+
+//Ex3
+function oneArg(num1) {
+    num1 * 100;
+    re
+}
