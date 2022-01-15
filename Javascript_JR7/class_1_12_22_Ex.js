2f166bd93abdeccdac5144c107fb4e9c785baa9d
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-15 10:10:07 -0700

    created file for class ex

---
+++ Javascript_JR7/class_1_12_22_Ex.js
@@ -0,0 +1,13 @@
+
+//PART1
+
+
+let animals = ['Monkey', 'Dolphine', 'Cat', 'Squirrel']
+
+function nameAnimals() {
+    for(i = 0; i <= 4; i++) {
+        console.log("Mommy, I want to see " + animals)
+}
+
+//PART2
+
