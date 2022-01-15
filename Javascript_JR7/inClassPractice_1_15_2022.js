177cd65cfee0a7b660e5d88c07af9658c99b7164
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-15 10:23:15 -0700

    completing class exercises

---
+++ Javascript_JR7/inClassPractice_1_15_2022.js
@@ -0,0 +1,26 @@
+//? Write Function that returns the string "Hello World"
+
+function worldFunct() {
+    return "Hello World";
+}
+
+console.log(worldFunct());
+
+//? Please write a function that takes in 1 string as an argument and returns that string
+
+var string = "ARC1JR is the best!";
+
+var stringFunction = function(string) {
+    console.log(string);
+}
+
+stringFunction(string);
+
+//? Please write a function that takes in 2 numbers as an argument and return the sum of those numbers
+//? Example => addNum(17,3) should return 20
+
+function twoNumber(num1, num2) {
+    console.log(num1 + num2);
+}
+
+twoNumber(17, 3)
