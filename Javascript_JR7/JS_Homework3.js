d3bf7b27a0582b264f26b2518deb9a45494a4c21
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2022-01-24 16:19:10 -0700

    file renamed

--- Javascript_JR7/JS_Homework3.js
+++ Javascript_JR7/JS_Homework3.js
@@ -2,3 +2,14 @@
 //Create a function that adds up the integers in the following array.
 
 var numbers = [10, 20, 30, 40, 50];
+
+function addNumbers(numberArray) {
+    var result = 0;
+    for(var i = 0; i < numbers.length; i++) {
+        result = result + numberArray(i);
+    }
+    return result;
+}
+
+console.log(result);
+
